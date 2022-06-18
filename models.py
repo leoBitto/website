ee4e92fe15ddeec6d0f94dcea546ed5a323e8571
leoBitto <leonardobitto@gmail.com>
2022-06-18 07:36:27 +0200

    added base.html

--- models.py
+++ models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 # Create your models here.
 class Info(models.Model):
-    email = models.CharField(
+    email = models.EmailField(
         max_length=40,
         null=True,
-        blank=True
+        blank=True,
     )
     photo = models.ImageField(
         upload_to='',
@@ -16,3 +16,6 @@ class Info(models.Model):
     date_updated = models.DateTimeField(
         auto_now=True,
     )
+
+    def __str__(self):
+        return 'info'
--- views.py
+++ views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Info
 
 # Create your views here.
+def index(request):
+    info = Info.objects.all()
+    context = {
+        'info': info
+    }
+
+    return render(request, 'site.html', context)
