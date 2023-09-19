a4fedc89aad21168e2d324fc80897455b0439545
leoBitto <leonardobitto@gmail.com>
2023-09-19 16:54:28 +0200

    modified footer and added models

--- admin.py
+++ admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Gallery_image,Contact,Opening_hour
 
-# Register your models here.
+
+class Gallery_imageAdmin(admin.ModelAdmin):
+    list_display=('id', 'description', 'is_first', 'show_image')
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'phone', 'mail')
+
+
+class OpeningHoursAdmin(admin.ModelAdmin):
+    list_display = ('id', 'weekdays', 'weekend', 'closing_day')
+
+
+admin.site.register(Gallery_image, Gallery_imageAdmin)
+admin.site.register(Contact, ContactAdmin)
+admin.site.register(Opening_hour, OpeningHoursAdmin)
